1059c05bdd39d124725f3b6f2a591f5b179ba5de
frankk <daffazaidan08@gmail.com>
2025-10-16 12:04:05 +0800

    Create TROC42_A.cpp

---
+++ TROC/TROC42_A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int x;
+    cin >> x;
+
+    if (x == 4) cout << "0\n";
+    else if (x == 3) cout << "6\n";
+    else if (x == 2) cout << "3\n";
+    else if (x == 1) cout << "1\n";
+
+    return 0;
+}
